c1a23d90da136ba5cf5d43a298cf59a345601095
xidian12 <xidian@xidian.edu.cn>
2026-02-03 15:47:44 +0800

    更新数据集导入，合并DIORDataset和DOTADataset到voc_abr模块

--- maskrcnn_benchmark/data/datasets/__init__.py
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -1,9 +1,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 from .coco import COCODataset
 from .concat_dataset import ConcatDataset
-from .voc import DIORDataset, DOTADataset
 from .voc2012_Instance import PascalVOCDataset2012
-from .voc_abr import PascalVOCDataset, PascalVOCDataset_ABR
+from .voc_abr import PascalVOCDataset, PascalVOCDataset_ABR, DIORDataset, DOTADataset
 
 __all__ = ["COCODataset", "ConcatDataset", "PascalVOCDataset", "PascalVOCDataset_ABR", "PascalVOCDataset2012",
            "DIORDataset", "DOTADataset"]
