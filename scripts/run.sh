5917226576b1afe436eab8ee54bb1993347a9130
xidian12 <xidian@xidian.edu.cn>
2026-02-08 15:02:00 +0800

    修正了训练脚本中的参数错误

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,7 +10,7 @@ shopt -s expand_aliases
 task=19-1
 exp -t ${task} -n ILOD
 exp -t ${task} -n FILOD --rpn --feat std --cls 1.
-exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
+exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1
 
 task=15-5
 exp -t ${task} -n ILOD
@@ -20,7 +20,7 @@ exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5
 task=10-10
 exp -t ${task} -n ILOD
 exp -t ${task} -n FILOD --feat std --rpn --cls 1.
-exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1
+exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
 
 task=5-15
 exp -t ${task} -n ILOD
@@ -32,7 +32,7 @@ alias exp_dota="exp -d DOTA"
 task=14-1
 exp_dota -t ${task} -n ILOD
 exp_dota -t ${task} -n FILOD --rpn --feat std --cls 1.
-exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
+exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1
 
 task=10-5
 exp_dota -t ${task} -n ILOD
@@ -42,7 +42,7 @@ exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5
 task=8-7
 exp_dota -t ${task} -n ILOD
 exp_dota -t ${task} -n FILOD --feat std --rpn --cls 1.
-exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1
+exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
 
 task=5-10
 exp_dota -t ${task} -n ILOD
