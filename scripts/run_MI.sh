deca8661979aaa04e350466ae0fc8eb4991d4a2e
xidian12 <xidian@xidian.edu.cn>
2026-02-26 17:02:16 +0800

    - 修改配置文件与训练脚本 - 更新数据集转换脚本

--- scripts/run_MI.sh
+++ scripts/run_MI.sh
@@ -2,47 +2,47 @@
 
 alias exp="python tools/train_incremental.py"
 
-task=10-2
-for s in {1..5}; do
-  exp -t ${task} -n ILOD -s $s
-  exp -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
-  exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1 -s $s
-done
-
-task=15-1
-for s in {1..5}; do
-  exp -t ${task} -n ILOD -s $s
-  exp -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
-  exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1 -s $s
-done
-
 task=10-5
 for s in {1..2}; do
   exp -t ${task} -n ILOD -s $s
   exp -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
-  exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5 -s $s
+  exp -t ${task} -n MMA -s $s --rpn --uce --dist_type uce --cls 0.5 SOLVER.BASE_LR 0.005
 done
 
 task=5-5
 for s in {1..3}; do
   exp -t ${task} -n ILOD -s $s
   exp -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
-  exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5 -s $s
+  exp -t ${task} -n MMA -s $s --rpn --uce --dist_type uce --cls 0.5 SOLVER.BASE_LR 0.005
+done
+
+task=10-2
+for s in {1..5}; do
+  exp -t ${task} -n ILOD -s $s
+  exp -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
+  exp -t ${task} -n MMA -s $s --rpn --uce --dist_type uce --cls 1. SOLVER.BASE_LR 0.001
+done
+
+task=15-1
+for s in {1..5}; do
+  exp -t ${task} -n ILOD -s $s
+  exp -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
+  exp -t ${task} -n MMA -s $s --rpn --uce --dist_type uce --cls 1.
 done
 
 
 alias exp_dota="exp -d DOTA"
 
+task=5-5
+for s in {1..2}; do
+  exp_dota -t ${task} -n ILOD -s $s
+  exp_dota -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
+  exp_dota -t ${task} -n MMA -s $s --rpn --uce --dist_type uce --cls 0.5 SOLVER.BASE_LR 0.005
+done
+
 task=10-1
 for s in {1..5}; do
   exp_dota -t ${task} -n ILOD -s $s
   exp_dota -t ${task} -n FILOD -s $s --rpn --feat std --cls 1.
-  exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1 -s $s
-done
-
-task=5-5
-for s in {1..2}; do
-  exp_dota -t ${task} -n ILOD -s $s
-  exp_dota -t ${task} -n FILOD -s $s --rpn --feat std --cls 1. SOLVER.BASE_LR 0.0005
-  exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5 -s $s
+  exp_dota -t ${task} -n MMA -s $s --rpn --uce --dist_type uce --cls 1.
 done
--- scripts/run_SI.sh
+++ scripts/run_SI.sh
@@ -5,41 +5,41 @@ alias exp="python tools/train_incremental.py"
 task=19-1
 exp -t ${task} -n ILOD
 exp -t ${task} -n FILOD --rpn --feat std --cls 1.
-exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1
+exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1.
 
 task=15-5
 exp -t ${task} -n ILOD
-exp -t ${task} -n FILOD --feat std --rpn --cls 1.
-exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5
+exp -t ${task} -n FILOD --rpn --feat std --cls 1.
+exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5 SOLVER.BASE_LR 0.005
 
 task=10-10
-exp -t ${task} -n ILOD SOLVER.BASE_LR 0.001
-exp -t ${task} -n FILOD --feat std --rpn --cls 1. SOLVER.BASE_LR 0.001
-exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
+exp -t ${task} -n ILOD
+exp -t ${task} -n FILOD --rpn --feat std --cls 1.
+exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1 SOLVER.BASE_LR 0.005
 
 task=5-15
-exp -t ${task} -n ILOD SOLVER.BASE_LR 0.001
-exp -t ${task} -n FILOD --feat std --rpn --cls 1. SOLVER.BASE_LR 0.001
-exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
+exp -t ${task} -n ILOD
+exp -t ${task} -n FILOD --rpn --feat std --cls 1.
+exp -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1 SOLVER.BASE_LR 0.005
 
 
 alias exp_dota="exp -d DOTA"
 task=14-1
 exp_dota -t ${task} -n ILOD
 exp_dota -t ${task} -n FILOD --rpn --feat std --cls 1.
-exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1
+exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 1.
 
 task=10-5
 exp_dota -t ${task} -n ILOD
-exp_dota -t ${task} -n FILOD --feat std --rpn --cls 1. SOLVER.BASE_LR 0.0005
-exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5
+exp_dota -t ${task} -n FILOD --rpn --feat std --cls 1.
+exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.5 SOLVER.BASE_LR 0.005
 
 task=8-7
-exp_dota -t ${task} -n ILOD SOLVER.BASE_LR 0.001
-exp_dota -t ${task} -n FILOD --feat std --rpn --cls 1. SOLVER.BASE_LR 0.001
-exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
+exp_dota -t ${task} -n ILOD
+exp_dota -t ${task} -n FILOD --rpn --feat std --cls 1.
+exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1 SOLVER.BASE_LR 0.01
 
 task=5-10
-exp_dota -t ${task} -n ILOD SOLVER.BASE_LR 0.001
-exp_dota -t ${task} -n FILOD --feat std --rpn --cls 1. SOLVER.BASE_LR 0.001
-exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1
+exp_dota -t ${task} -n ILOD
+exp_dota -t ${task} -n FILOD --rpn --feat std --cls 1.
+exp_dota -t ${task} -n MMA --rpn --uce --dist_type uce --cls 0.1 SOLVER.BASE_LR 0.01
